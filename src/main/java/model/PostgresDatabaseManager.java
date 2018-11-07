6fbbbf11dadeb385751b3dea778668379b2bb70f
Andrey Kukuruza <andrey.kykyryza@gmail.com>
2018-11-07 11:53:37 +0200

    Implemented function find. Remove method find from DatabaseManager and PostgresDatabaseManger and added methods getTableData(), getColumnNamesInTable(). Command Find use this methods and class-helper TableFormatter for printing table in console. Added dependency Log4j and created file log4j.properties, because TableFormatter writing warning in console without this.

--- src/main/java/model/PostgresDatabaseManager.java
+++ src/main/java/model/PostgresDatabaseManager.java
@@ -1,8 +1,7 @@
 package model;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PostgresDatabaseManager implements DatabaseManager {
     private Connection connection;
@@ -43,39 +42,35 @@ public class PostgresDatabaseManager implements DatabaseManager {
 
     @Override
     public ArrayList<String> tables() throws SQLException {
-        if (isConnected()) {
-            String sql = "SELECT table_name FROM information_schema.tables\n" +
-                    "WHERE table_schema NOT IN ('information_schema', 'pg_catalog')\n" +
-                    "AND table_schema IN('public');";
-            ArrayList<String> tableNames = new ArrayList<>();
+        throwExceptionIfNotConnected();
 
-            try (Statement statement = connection.createStatement();
-                 ResultSet resultSet = statement.executeQuery(sql)) {
-                while (resultSet.next()) {
-                    tableNames.add(resultSet.getString(1));
-                }
-                return tableNames;
+        String sql = "SELECT table_name FROM information_schema.tables\n" +
+                "WHERE table_schema NOT IN ('information_schema', 'pg_catalog')\n" +
+                "AND table_schema IN('public');";
+        ArrayList<String> tableNames = new ArrayList<>();
+
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                tableNames.add(resultSet.getString(1));
             }
-        } else {
-            throw new SQLException(youAreNotConnectedToDatabaseErrorMessage);
+            return tableNames;
         }
     }
 
     @Override
     public void clear(String tableName) throws SQLException {
-        if (isConnected()) {
-            try (Statement statement = connection.createStatement()) {
-                String sql = "DELETE FROM " + tableName + ";";
-                statement.executeUpdate(sql);
-            } catch (SQLException e) {
-                if (e.getMessage().equals("ERROR: relation \"" + tableName + "\" does not exist\n" + "  Позиция: 13")) {
-                    throw new SQLException("Table " + tableName + " does not exist.", e);
-                } else {
-                    throw e;
-                }
+        throwExceptionIfNotConnected();
+
+        try (Statement statement = connection.createStatement()) {
+            String sql = "DELETE FROM " + tableName + ";";
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            if (e.getMessage().equals("ERROR: relation \"" + tableName + "\" does not exist\n" + "  Позиция: 13")) {
+                throw new SQLException("Table " + tableName + " does not exist.", e);
+            } else {
+                throw e;
             }
-        } else {
-            throw new SQLException(youAreNotConnectedToDatabaseErrorMessage);
         }
     }
 
@@ -106,8 +101,54 @@ public class PostgresDatabaseManager implements DatabaseManager {
     }
 
     @Override
-    public void find(String command) {
+    public List<String> getTableData(String tableName) throws SQLException {
+        throwExceptionIfNotConnected();
 
+        String sql = "SELECT * FROM public." + tableName + ";";
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            List<String> result = new ArrayList<>();
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            while (resultSet.next()) {
+                for (int i = 0; i < resultSetMetaData.getColumnCount(); i++) {
+                    result.add(resultSet.getObject(i + 1).toString());
+                }
+            }
+
+            return result;
+        } catch (SQLException e) {
+            if (e.getMessage().startsWith("ERROR: relation \"public." + tableName + "\" does not exist")) {
+                throw new SQLException("Table " + tableName + " does not exist.");
+            } else if (e.getMessage().startsWith("ERROR: syntax error at or near")) {
+                throw new SQLException("Syntax error in table name.");
+            }
+            throw e;
+        }
+    }
+
+    @Override
+    public Set<String> getColumnNamesInTable(String tableName) throws SQLException {
+        throwExceptionIfNotConnected();
+
+        String sql = "SELECT * FROM public." + tableName + ";";
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            Set<String> result = new LinkedHashSet<>();
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+
+            for (int i = 0; i < resultSetMetaData.getColumnCount(); i++) {
+                result.add(resultSetMetaData.getColumnName(i + 1));
+            }
+
+            return result;
+        } catch (SQLException e) {
+            if (e.getMessage().startsWith("ERROR: relation \"public." + tableName + "\" does not exist")) {
+                throw new SQLException("Table " + tableName + " does not exist.");
+            } else if (e.getMessage().startsWith("ERROR: syntax error at or near")) {
+                throw new SQLException("Syntax error in table name.");
+            }
+            throw e;
+        }
     }
 
     @Override
@@ -115,23 +156,22 @@ public class PostgresDatabaseManager implements DatabaseManager {
         if (namesOfColumns.size() != typesOfColumns.size()) {
             throw new IllegalArgumentException("Can not create new table with difference number of column names and column types.");
         }
-        if (isConnected()) {
-            try (Statement statement = connection.createStatement()) {
-                String sql = getSQLForCreatingNewTable(tableName, namesOfColumns, typesOfColumns);
-                statement.executeUpdate(sql);
-            } catch (SQLException e) {
-                if (e.getMessage().equals("ERROR: relation \"test\" already exists")) {
-                    throw new SQLException("Table with name " + tableName + " already exists. You can not create the table with the same name.", e);
-                } else if (e.getMessage().startsWith("ERROR: type")) {
-                    throw new SQLException("You used wrong type for one or more columns.", e);
-                } else if(e.getMessage().startsWith("ERROR: syntax error at or near")) {
-                    throw new SQLException("You used wrong name for one or more columns.", e);
-                }else {
-                    throw e;
-                }
+
+        throwExceptionIfNotConnected();
+
+        try (Statement statement = connection.createStatement()) {
+            String sql = getSQLForCreatingNewTable(tableName, namesOfColumns, typesOfColumns);
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            if (e.getMessage().equals("ERROR: relation \"test\" already exists")) {
+                throw new SQLException("Table with name " + tableName + " already exists. You can not create the table with the same name.", e);
+            } else if (e.getMessage().startsWith("ERROR: type")) {
+                throw new SQLException("You used wrong type for one or more columns.", e);
+            } else if (e.getMessage().startsWith("ERROR: syntax error at or near")) {
+                throw new SQLException("You used wrong name for one or more columns.", e);
+            } else {
+                throw e;
             }
-        } else {
-            throw new SQLException(youAreNotConnectedToDatabaseErrorMessage);
         }
     }
 
@@ -150,17 +190,21 @@ public class PostgresDatabaseManager implements DatabaseManager {
 
     @Override
     public void drop(String tableName) throws SQLException {
-        if (isConnected()) {
-            try (Statement statement = connection.createStatement()) {
-                String sql = "DROP TABLE " + tableName + ";";
-                statement.executeUpdate(sql);
-            } catch (SQLException e) {
-                if (e.getMessage().equals("ERROR: table \"" + tableName + "\" does not exist")) {
-                    throw new SQLException("Table " + tableName + " does not exist.");
-                }
-                System.out.println(e.getMessage());
+        throwExceptionIfNotConnected();
+
+        try (Statement statement = connection.createStatement()) {
+            String sql = "DROP TABLE " + tableName + ";";
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            if (e.getMessage().equals("ERROR: table \"" + tableName + "\" does not exist")) {
+                throw new SQLException("Table " + tableName + " does not exist.");
             }
-        } else {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void throwExceptionIfNotConnected() throws SQLException {
+        if (!isConnected()) {
             throw new SQLException(youAreNotConnectedToDatabaseErrorMessage);
         }
     }
