2e2aa65d2cc608c2dbbb9a426a669274ec89a6c0
Andrey Kukuruza <andrey.kykyryza@gmail.com>
2018-12-01 22:51:43 +0200

    Added tests for Exit command

---
+++ src/test/java/controller/command/ExitTest.java
@@ -0,0 +1,53 @@
+package controller.command;
+
+import model.DatabaseManager;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import view.View;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ExitTest {
+    @Mock
+    private View view;
+    @Mock
+    private DatabaseManager manager;
+    @InjectMocks
+    private Exit exit = new Exit(view, manager);
+
+    @Test
+    void canExecuteTest_WithCorrectCommandName() {
+        assertTrue(exit.canExecute("exit"));
+    }
+
+    @Test
+    void canExecuteTest_WithIncorrectCommandName() {
+        assertFalse(exit.canExecute("WrongCommandName"));
+    }
+
+    @Test
+    void executeTest_WithCorrectConnectionClosing() {
+//        when
+        exit.execute("exit");
+//        then
+        verify(view, times(1)).write("Goodbye!");
+    }
+
+    @Test
+    void executeTest_WithIncorrectConnectionClosing() throws SQLException {
+//        given
+        doThrow(new SQLException("Connection was not close.")).when(manager).exit();
+//        when
+        exit.execute("exit");
+//        then
+        verify(view, times(1)).write("Connection was not close. Goodbye!");
+    }
+}
