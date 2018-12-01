798bd8a1f8f137ce4756a155441552611e0119e6
Andrey Kukuruza <andrey.kykyryza@gmail.com>
2018-12-01 18:23:47 +0200

    Added tests for Create command

---
+++ src/test/java/controller/command/CreateTest.java
@@ -0,0 +1,116 @@
+package controller.command;
+
+import controller.command.util.CommandMessages;
+import model.DatabaseManager;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import view.View;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CreateTest {
+    @Mock
+    private View view;
+    @Mock
+    private DatabaseManager manager;
+    @InjectMocks
+    private Create create = new Create(view, manager);
+
+    @Test
+    void canExecuteTest_WithCorrectCommandName() {
+        assertTrue(create.canExecute("create"));
+    }
+
+    @Test
+    void canExecuteTest_WithIncorrectCommandName() {
+        assertFalse(create.canExecute("WrongCommandName"));
+    }
+
+    @Test
+    void executeTest_WithCorrectParametersInCommand() throws SQLException {
+//        given
+        doNothing().when(manager).create(anyString(), anyList(), anyList());
+        doNothing().when(view).write(anyString());
+//        when
+        create.execute("create|TableName|ColumnName1|ColumnValue1|ColumnName2|ColumnValue2");
+//        then
+        verify(manager, times(1))
+                .create("TableName",
+                        Arrays.asList("ColumnName1", "ColumnName2"),
+                        Arrays.asList("ColumnValue1", "ColumnValue2"));
+        verify(view, times(1)).write("Table TableName was created. Enter next command or help:");
+    }
+
+    @Test
+    void executeTest_WithDifferentNumberOfColumnsNamesAndTypes() {
+//        given
+        doNothing().when(view).write(anyString());
+//        when
+        create.execute("create|TableName|ColumnName1|ColumnValue1|ColumnName2");
+//        then
+        verify(view, times(1)).write(CommandMessages.INCORRECT_FORMAT_ERR_MSG);
+    }
+
+    @Test
+    void executeTest_WithoutColumns() throws SQLException {
+//        given
+        doNothing().when(view).write(anyString());
+        doNothing().when(manager).create(anyString(), anyList(), anyList());
+//        when
+        create.execute("create|TableName");
+//        then
+        verify(manager, times(1))
+                .create("TableName", Collections.emptyList(), Collections.emptyList());
+        verify(view, times(1)).write("Table TableName was created. Enter next command or help:");
+    }
+
+    @Test
+    void executeTest_WithoutTableName() {
+//        given
+        doNothing().when(view).write(anyString());
+//        when
+        create.execute("create|ColumnName1|ColumnValue1|ColumnName2|ColumnValue2");
+//        then
+        verify(view, times(1)).write(CommandMessages.INCORRECT_FORMAT_ERR_MSG);
+    }
+
+    @Test
+    void executeTest_WithIncorrectTableName() throws SQLException {
+//        given
+        doNothing().when(view).write(anyString());
+        SQLException e = new SQLException("Input data does not correct.");
+        doThrow(e).when(manager).create("WrongTableName",
+                Arrays.asList("ColumnName1", "ColumnName2"),
+                Arrays.asList("ColumnValue1", "ColumnValue2"));
+//        when
+        create.execute("create|WrongTableName|ColumnName1|ColumnValue1|ColumnName2|ColumnValue2");
+//        then
+        verify(manager, times(1)).create("WrongTableName",
+                Arrays.asList("ColumnName1", "ColumnName2"),
+                Arrays.asList("ColumnValue1", "ColumnValue2"));
+        verify(view, times(1)).write(e.getMessage());
+        verify(view, times(1)).write(CommandMessages.ENTER_NEXT_COMMAND);
+    }
+
+    @Test
+    void executeTest_WithoutParameters() {
+//        given
+        doNothing().when(view).write(anyString());
+//        when
+        create.execute("create");
+//        then
+        verify(view, times(1)).write(CommandMessages.INCORRECT_FORMAT_ERR_MSG);
+    }
+}
