bbaa414a0001863ac29345d5675e50a7a101a525
Andrey Kukuruza <andrey.kykyryza@gmail.com>
2018-12-02 17:08:23 +0200

    Added tests for Unsupported command

---
+++ src/test/java/controller/command/UnsupportedTest.java
@@ -0,0 +1,34 @@
+package controller.command;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import view.View;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class UnsupportedTest {
+    @Mock
+    private View view;
+    @InjectMocks
+    private Unsupported unsupported = new Unsupported(view);
+
+    @Test
+    void canExecuteTest() {
+        assertTrue(unsupported.canExecute("AnyString"));
+    }
+
+    @Test
+    void executeTest() {
+//        when
+        unsupported.execute("anyString");
+//        then
+        verify(view, times(1))
+                .write("Wrong command! Please enter correct command or help:");
+    }
+}
