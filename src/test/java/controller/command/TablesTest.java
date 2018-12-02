8f958c185a285175920bec18d651daf3d35c58ed
Andrey Kukuruza <andrey.kykyryza@gmail.com>
2018-12-02 16:56:51 +0200

    Added tests for Tables command

---
+++ src/test/java/controller/command/TablesTest.java
@@ -0,0 +1,74 @@
+package controller.command;
+
+import controller.command.util.CommandMessages;
+import model.DatabaseManager;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import view.View;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class TablesTest {
+    @Mock
+    private View view;
+    @Mock
+    private DatabaseManager manager;
+    @InjectMocks
+    private Tables tables = new Tables(view, manager);
+
+    @Test
+    void canExecuteTest_WithCorrectCommandName() {
+        assertTrue(tables.canExecute("tables"));
+    }
+
+    @Test
+    void canExecuteTest_WithIncorrectCommandName() {
+        assertFalse(tables.canExecute("WrongCommandName"));
+    }
+
+    @Test
+    void executeTest_WithSomeTablesInDatabase() throws SQLException {
+//        given
+        when(manager.tables()).thenReturn(new HashSet<>(Arrays.asList("TableName1", "TableName2")));
+//        when
+        tables.execute("tables");
+//        then
+        verify(manager, times(1)).tables();
+        verify(view, times(1)).write(contains("TableName1"));
+        verify(view, times(1)).write(contains("TableName2"));
+        verify(view, times(1)).write(CommandMessages.ENTER_NEXT_COMMAND);
+    }
+
+    @Test
+    void executeTest_WithoutTablesInDatabase() throws SQLException {
+//        given
+        when(manager.tables()).thenReturn(new HashSet<>());
+//        when
+        tables.execute("tables");
+//        then
+        verify(manager, times(1)).tables();
+        verify(view, times(1)).write("No tables inside the database.");
+        verify(view, times(1)).write(CommandMessages.ENTER_NEXT_COMMAND);
+    }
+
+    @Test
+    void executeTest_IfException() throws SQLException {
+//        given
+        when(manager.tables()).thenThrow(new SQLException("Any error message"));
+//        when
+        tables.execute("tables");
+//        then
+        verify(manager, times(1)).tables();
+        verify(view, times(2)).write(anyString());
+    }
+}
