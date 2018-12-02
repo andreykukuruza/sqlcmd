683ff9b54bbf562c6856bcafab98a0852814ea0b
Andrey Kukuruza <andrey.kykyryza@gmail.com>
2018-12-02 16:18:21 +0200

    Added tests for helper command isConnected

---
+++ src/test/java/controller/command/IsConnectedTest.java
@@ -0,0 +1,49 @@
+package controller.command;
+
+import model.DatabaseManager;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import view.View;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class IsConnectedTest {
+    @Mock
+    private View view;
+    @Mock
+    private DatabaseManager manager;
+    @InjectMocks
+    private IsConnected isConnected = new IsConnected(view, manager);
+
+    @Test
+    void canExecuteTest_IfConnected() {
+//        given
+        when(manager.isConnected()).thenReturn(true);
+//        then
+        assertFalse(isConnected.canExecute("isConnected"));
+    }
+
+    @Test
+    void canExecuteTest_IfNotConnected() {
+//        given
+        when(manager.isConnected()).thenReturn(false);
+//        then
+        assertTrue(isConnected.canExecute("isConnected"));
+    }
+
+    @Test
+    void executeTest() {
+//        when
+        isConnected.execute("isConnected");
+//        then
+        verify(view)
+                .write("You can not use this command before connect to database. Enter next command or help:");
+    }
+}
