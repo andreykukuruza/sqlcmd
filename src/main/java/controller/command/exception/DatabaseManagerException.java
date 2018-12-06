967a1179248a6926e70615f25651a7bc188dd5f0
Andrey Kukuruza <andrey.kykyryza@gmail.com>
2018-12-06 10:36:03 +0200

    Renamed InputDataIncorrectException to DatabaseManagerException

---
+++ src/main/java/controller/command/exception/DatabaseManagerException.java
@@ -0,0 +1,15 @@
+package controller.command.exception;
+
+public class DatabaseManagerException extends RuntimeException {
+    public DatabaseManagerException(String message, Exception cause) {
+        super(message, cause);
+    }
+
+    public DatabaseManagerException(String message) {
+        super(message);
+    }
+
+    public DatabaseManagerException() {
+        super();
+    }
+}
