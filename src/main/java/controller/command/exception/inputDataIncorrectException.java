6438d830582a198498133924b517d95563ef08cb
Andrey Kukuruza <andrey.kykyryza@gmail.com>
2018-12-06 10:17:45 +0200

    Added different constructors to inputDataIncorrectException

--- src/main/java/controller/command/exception/inputDataIncorrectException.java
+++ src/main/java/controller/command/exception/inputDataIncorrectException.java
@@ -1,4 +1,15 @@
 package controller.command.exception;
 
 public class inputDataIncorrectException extends RuntimeException {
+    public inputDataIncorrectException(String message, Exception cause) {
+        super(message, cause);
+    }
+
+    public inputDataIncorrectException(String message) {
+        super(message);
+    }
+
+    public inputDataIncorrectException() {
+        super();
+    }
 }
