3393c505466decd3724df3767821face0d2e4a36
Andrey Kukuruza <andrey.kykyryza@gmail.com>
2018-12-06 17:33:19 +0200

    Removed excess import.

--- src/test/java/controller/command/HelpTest.java
+++ src/test/java/controller/command/HelpTest.java
@@ -8,8 +8,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import view.View;
 
-import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
