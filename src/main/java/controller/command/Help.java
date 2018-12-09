061315eed94a9eae0b306b8f31d68d79a5a973af
Andrey Kukuruza <andrey.kykyryza@gmail.com>
2018-12-09 20:53:04 +0200

    Removed excess import

--- src/main/java/controller/command/Help.java
+++ src/main/java/controller/command/Help.java
@@ -6,7 +6,6 @@ import view.View;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Help implements Command {
