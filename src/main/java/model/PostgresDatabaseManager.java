d9565cfee4314ed69b2c1d2e89390064969120fe
Andrey Kukuruza <andrey.kykyryza@gmail.com>
2018-12-10 13:52:23 +0200

    Extracted sql queries from PostgresDatabaseManager to PostgresSqlQueries

--- src/main/java/model/PostgresDatabaseManager.java
+++ src/main/java/model/PostgresDatabaseManager.java
@@ -5,9 +5,16 @@ import model.exception.DatabaseManagerException;
 import java.sql.*;
 import java.util.*;
 
+import static model.sqlQueries.PostgresSqlQueries.*;
+
 public class PostgresDatabaseManager implements DatabaseManager {
     private Connection connection;
-    private String incorrectInputDataErrorMessage = "Input data does not correct.";
+    private static final String WRONG_INPUT_DATA_ERR_MSG = "Input data does not correct.";
+    private static final String WRONG_NAME_OR_PASSWORD_ERR_MSG = "Username or password are incorrect.";
+    private static final String WRONG_DB_NAME_ERR_MSG = "Database %s does not exist.";
+    private static final String CLOSE_CONNECTION_ERR_MSG = "Connection was not close.";
+    private static final String NEED_CONNECTION_ERR_MSG = "You are not connected to database.";
+    private static final String UNKNOWN_ERR_MSG = "Houston, we have a problem!...";
 
     @Override
     public boolean isConnected() {
@@ -17,29 +24,25 @@ public class PostgresDatabaseManager implements DatabaseManager {
     @Override
     public void connect(String databaseName, String userName, String password) {
         closeIfWasConnected();
-        String url = "jdbc:postgresql://localhost:5432/" + databaseName;
+        String url = String.format(URL, databaseName);
         try {
             connection = DriverManager.getConnection(url, userName, password);
         } catch (SQLException e) {
-            if (e.getMessage().equals("FATAL: password authentication failed for user \"" + userName + "\"")) {
-                throw new DatabaseManagerException("Username or password are incorrect.", e);
-            } else if (e.getMessage().equals("FATAL: database \"" + databaseName + "\" does not exist")) {
-                throw new DatabaseManagerException("Database " + databaseName + " does not exist.", e);
-            } else {
-                throw new DatabaseManagerException("Houston, we have a problem!...", e);
+            if (e.getMessage().equals(String.format(NAME_OR_PASS_WRONG_ERR_MSG, userName))) {
+                throw new DatabaseManagerException(WRONG_NAME_OR_PASSWORD_ERR_MSG, e);
+            } else if (e.getMessage().equals(String.format(WRONG_DATABASE_NAME_ERR_MSG, databaseName))) {
+                throw new DatabaseManagerException(String.format(WRONG_DB_NAME_ERR_MSG, databaseName), e);
             }
+            throw new DatabaseManagerException(UNKNOWN_ERR_MSG, e);
         }
     }
 
     @Override
     public Set<String> tables() {
         throwExceptionIfNotConnected();
-        String sql = "SELECT table_name FROM information_schema.tables\n" +
-                "WHERE table_schema NOT IN ('information_schema', 'pg_catalog')\n" +
-                "AND table_schema IN('public');";
         Set<String> tableNames = new HashSet<>();
         try (Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(sql)) {
+             ResultSet resultSet = statement.executeQuery(GET_ALL_TABLES_NAMES)) {
             while (resultSet.next()) {
                 tableNames.add(resultSet.getString(1));
             }
@@ -53,10 +56,10 @@ public class PostgresDatabaseManager implements DatabaseManager {
     public void clear(String tableName) {
         throwExceptionIfNotConnected();
         try (Statement statement = connection.createStatement()) {
-            String sql = "DELETE FROM " + tableName + ";";
+            String sql = String.format(CLEAR_TABLE, tableName);
             statement.executeUpdate(sql);
         } catch (SQLException e) {
-            throw new DatabaseManagerException(incorrectInputDataErrorMessage, e);
+            throw new DatabaseManagerException(WRONG_INPUT_DATA_ERR_MSG, e);
         }
     }
 
@@ -66,7 +69,7 @@ public class PostgresDatabaseManager implements DatabaseManager {
             try {
                 connection.close();
             } catch (SQLException e) {
-                throw new DatabaseManagerException("Connection was not close.", e);
+                throw new DatabaseManagerException(CLOSE_CONNECTION_ERR_MSG, e);
             }
         }
     }
@@ -75,11 +78,11 @@ public class PostgresDatabaseManager implements DatabaseManager {
     public void delete(String tableName, String verifiableColumnName, String verifiableColumnValue) {
         throwExceptionIfNotConnected();
         try (Statement statement = connection.createStatement()) {
-            String sql = "DELETE FROM " + tableName + " WHERE " + verifiableColumnName + " = "
-                    + verifiableColumnValue + ";";
+            String sql = String.format(DELETE_FROM_TABLE_ROWS,
+                    tableName, verifiableColumnName, verifiableColumnValue);
             statement.executeUpdate(sql);
         } catch (SQLException e) {
-            throw new DatabaseManagerException(incorrectInputDataErrorMessage, e);
+            throw new DatabaseManagerException(WRONG_INPUT_DATA_ERR_MSG, e);
         }
     }
 
@@ -92,7 +95,7 @@ public class PostgresDatabaseManager implements DatabaseManager {
                     verifiableColumnValue, columnNameToColumnValueOfUpdatableRows);
             statement.executeUpdate(sql);
         } catch (SQLException e) {
-            throw new DatabaseManagerException(incorrectInputDataErrorMessage, e);
+            throw new DatabaseManagerException(WRONG_INPUT_DATA_ERR_MSG, e);
         }
     }
 
@@ -104,14 +107,14 @@ public class PostgresDatabaseManager implements DatabaseManager {
             String sql = getSQLForInsertDataInTable(tableName, columnNameToColumnValue);
             statement.executeUpdate(sql);
         } catch (SQLException e) {
-            throw new DatabaseManagerException(incorrectInputDataErrorMessage, e);
+            throw new DatabaseManagerException(WRONG_INPUT_DATA_ERR_MSG, e);
         }
     }
 
     @Override
     public List<String> getTableData(String tableName) {
         throwExceptionIfNotConnected();
-        String sql = "SELECT * FROM public." + tableName + ";";
+        String sql = String.format(SELECT_ALL_DATA_IN_TABLE, tableName);
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(sql)) {
             List<String> result = new ArrayList<>();
@@ -123,14 +126,14 @@ public class PostgresDatabaseManager implements DatabaseManager {
             }
             return result;
         } catch (SQLException e) {
-            throw new DatabaseManagerException(incorrectInputDataErrorMessage, e);
+            throw new DatabaseManagerException(WRONG_INPUT_DATA_ERR_MSG, e);
         }
     }
 
     @Override
     public Set<String> getColumnsNamesInTable(String tableName) {
         throwExceptionIfNotConnected();
-        String sql = "SELECT * FROM public." + tableName + ";";
+        String sql = String.format(SELECT_ALL_DATA_IN_TABLE, tableName);
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(sql)) {
             Set<String> result = new LinkedHashSet<>();
@@ -140,7 +143,7 @@ public class PostgresDatabaseManager implements DatabaseManager {
             }
             return result;
         } catch (SQLException e) {
-            throw new DatabaseManagerException(incorrectInputDataErrorMessage, e);
+            throw new DatabaseManagerException(WRONG_INPUT_DATA_ERR_MSG, e);
         }
     }
 
@@ -151,7 +154,7 @@ public class PostgresDatabaseManager implements DatabaseManager {
             String sql = getSQLForCreatingNewTable(tableName, columnNameToColumnType);
             statement.executeUpdate(sql);
         } catch (SQLException e) {
-            throw new DatabaseManagerException(incorrectInputDataErrorMessage, e);
+            throw new DatabaseManagerException(WRONG_INPUT_DATA_ERR_MSG, e);
         }
     }
 
@@ -159,66 +162,16 @@ public class PostgresDatabaseManager implements DatabaseManager {
     public void drop(String tableName) {
         throwExceptionIfNotConnected();
         try (Statement statement = connection.createStatement()) {
-            String sql = "DROP TABLE " + tableName + ";";
+            String sql = String.format(DROP_TABLE, tableName);
             statement.executeUpdate(sql);
         } catch (SQLException e) {
-            throw new DatabaseManagerException(incorrectInputDataErrorMessage, e);
+            throw new DatabaseManagerException(WRONG_INPUT_DATA_ERR_MSG, e);
         }
-    }
-
-    private String getSQLForCreatingNewTable(String tableName, Map<String, String> columnNameToColumnType) {
-        StringBuilder sql = new StringBuilder("CREATE TABLE public." + tableName + " (");
-        if (columnNameToColumnType.size() != 0) {
-            Iterator<Map.Entry<String, String>> iterator = columnNameToColumnType.entrySet().iterator();
-            while (iterator.hasNext()) {
-                Map.Entry<String, String> next = iterator.next();
-                sql.append(next.getKey()).append(" ").append(next.getValue());
-                if (iterator.hasNext()) {
-                    sql.append(", ");
-                }
-            }
-        }
-        return sql.append(");").toString();
-    }
-
-    private String getSQLForInsertDataInTable(String tableName, Map<String, String> columnNameToColumnValue) {
-        StringBuilder sql = new StringBuilder("INSERT INTO " + tableName + " (");
-        Iterator<String> keyIterator = columnNameToColumnValue.keySet().iterator();
-        while (keyIterator.hasNext()) {
-            sql.append(keyIterator.next());
-            if (keyIterator.hasNext()) {
-                sql.append(", ");
-            }
-        }
-        sql.append(") VALUES (");
-        Iterator<String> valueIterator = columnNameToColumnValue.values().iterator();
-        while (valueIterator.hasNext()) {
-            sql.append(valueIterator.next());
-            if (valueIterator.hasNext()) {
-                sql.append(", ");
-            }
-        }
-        return sql.append(");").toString();
-    }
-
-    private String getSQLForUpdatingData(String tableName, String verifiableColumnName,
-                                         String verifiableColumnValue, Map<String, String> namesToValuesOfUpdatableRow) {
-        StringBuilder sql = new StringBuilder("UPDATE " + tableName + " SET ");
-        Iterator<Map.Entry<String, String>> iterator = namesToValuesOfUpdatableRow.entrySet().iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<String, String> next = iterator.next();
-            sql.append(next.getKey()).append(" = ").append(next.getValue());
-            if (iterator.hasNext()) {
-                sql.append(", ");
-            }
-        }
-        sql.append(" WHERE ").append(verifiableColumnName).append(" = ").append(verifiableColumnValue).append(";");
-        return sql.toString();
     }
 
     private void throwExceptionIfNotConnected() {
         if (!isConnected()) {
-            throw new DatabaseManagerException("You are not connected to database.");
+            throw new DatabaseManagerException(NEED_CONNECTION_ERR_MSG);
         }
     }
 
@@ -227,7 +180,7 @@ public class PostgresDatabaseManager implements DatabaseManager {
             try {
                 connection.close();
             } catch (SQLException e) {
-                throw new DatabaseManagerException("Previous connection was not close. Try again or reset SQLCmd.", e);
+                throw new DatabaseManagerException(CLOSE_CONNECTION_ERR_MSG, e);
             }
         }
     }
---
+++ src/main/java/model/sqlQueries/PostgresSqlQueries.java
@@ -0,0 +1,74 @@
+package model.sqlQueries;
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class PostgresSqlQueries {
+    public static final String URL = "jdbc:postgresql://localhost:5432/%s";
+
+    public static final String GET_ALL_TABLES_NAMES = "SELECT table_name FROM information_schema.tables\n" +
+            "WHERE table_schema NOT IN ('information_schema', 'pg_catalog')\n" +
+            "AND table_schema IN('public');";
+    public static final String CLEAR_TABLE = "DELETE FROM %s;";
+    public static final String DELETE_FROM_TABLE_ROWS = "DELETE FROM %s WHERE %s = %s;";
+    public static final String SELECT_ALL_DATA_IN_TABLE = "SELECT * FROM public.%s;";
+    public static final String DROP_TABLE = "DROP TABLE %s;";
+
+    public static final String NAME_OR_PASS_WRONG_ERR_MSG = "FATAL: password authentication failed for user \"%s\"";
+    public static final String WRONG_DATABASE_NAME_ERR_MSG = "FATAL: database \"%s\" does not exist";
+
+    private static final String CREATE = "CREATE TABLE public.%s (";
+    private static final String INSERT = "INSERT INTO %s (";
+    private static final String UPDATE = "UPDATE %s SET ";
+
+    public static String getSQLForCreatingNewTable(String tableName, Map<String, String> columnNameToColumnType) {
+        StringBuilder sql = new StringBuilder(String.format(CREATE, tableName));
+        if (columnNameToColumnType.size() != 0) {
+            Iterator<Map.Entry<String, String>> iterator = columnNameToColumnType.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<String, String> next = iterator.next();
+                sql.append(next.getKey()).append(" ").append(next.getValue());
+                if (iterator.hasNext()) {
+                    sql.append(", ");
+                }
+            }
+        }
+        return sql.append(");").toString();
+    }
+
+    public static String getSQLForInsertDataInTable(String tableName, Map<String, String> columnNameToColumnValue) {
+        StringBuilder sql = new StringBuilder(String.format(INSERT, tableName));
+        Iterator<String> keyIterator = columnNameToColumnValue.keySet().iterator();
+        while (keyIterator.hasNext()) {
+            sql.append(keyIterator.next());
+            if (keyIterator.hasNext()) {
+                sql.append(", ");
+            }
+        }
+        sql.append(") VALUES (");
+        Iterator<String> valueIterator = columnNameToColumnValue.values().iterator();
+        while (valueIterator.hasNext()) {
+            sql.append(valueIterator.next());
+            if (valueIterator.hasNext()) {
+                sql.append(", ");
+            }
+        }
+        return sql.append(");").toString();
+    }
+
+    public static String getSQLForUpdatingData(String tableName, String verifiableColumnName,
+                                               String verifiableColumnValue,
+                                               Map<String, String> namesToValuesOfUpdatableRow) {
+        StringBuilder sql = new StringBuilder(String.format(UPDATE, tableName));
+        Iterator<Map.Entry<String, String>> iterator = namesToValuesOfUpdatableRow.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> next = iterator.next();
+            sql.append(next.getKey()).append(" = ").append(next.getValue());
+            if (iterator.hasNext()) {
+                sql.append(", ");
+            }
+        }
+        sql.append(" WHERE ").append(verifiableColumnName).append(" = ").append(verifiableColumnValue).append(";");
+        return sql.toString();
+    }
+}
