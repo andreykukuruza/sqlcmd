59f4be24922da16c19d8629fc584d29d865b57b1
Andrey Kukuruza <andrey.kykyryza@gmail.com>
2018-12-12 11:11:06 +0200

    Extracted command messages to CommandMessages

--- src/main/java/controller/command/SystemCommand.java
+++ src/main/java/controller/command/SystemCommand.java
@@ -1,15 +1,17 @@
 package controller.command;
 
+import static controller.command.util.CommandMessages.UNSUPPORTED_OPERATION_ERR_MSG;
+
 public abstract class SystemCommand implements Command {
     @Override
     public String format() {
         throw new UnsupportedOperationException(
-                String.format("%s is inner helper command without format", this.getClass().getSimpleName()));
+                String.format(UNSUPPORTED_OPERATION_ERR_MSG, this.getClass().getSimpleName()));
     }
 
     @Override
     public String description() {
         throw new UnsupportedOperationException(
-                String.format("%s is inner helper command without description", this.getClass().getSimpleName()));
+                String.format(UNSUPPORTED_OPERATION_ERR_MSG, this.getClass().getSimpleName()));
     }
 }
